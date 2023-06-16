446086ce33bad624b65f44e1c3d883c3980075d6
xc383 <xc383@drexel.edu>
2023-06-15 21:48:46 -0400

    Initial commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -10,7 +10,7 @@ object main extends IOApp {
   val month = 2
   val urlBase = "https://transtats.bts.gov/PREZIP/"
   val urlFile = s"On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${year}_$month.zip"
-  val outBase = "/Users/jxan/Documents/development/scala/DestinationsUS/resources/data/"
+  val outBase = "./DestinationsUS/resources/data/"
   val outZip = "files.zip"
   val outFile = "outs/"
   val target = s"On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_${year}_$month.csv"
