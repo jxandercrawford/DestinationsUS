ef3226a975b89c1f082040aa859131edd37d0cdb
xc383 <xc383@drexel.edu>
2023-06-17 01:43:19 -0400

    Added config file

---
+++ src/main/scala/config.scala
@@ -0,0 +1,17 @@
+import java.io.File
+import java.net.URL
+import java.util.*
+import scala.io.Source
+
+object config {
+  private val propertiesPath = new File(".").getCanonicalPath + "/resources/application.properties"
+  def getConfig: Either[Throwable, Properties] =
+    val properties: Properties = new Properties()
+
+    try
+      val source = Source.fromFile(propertiesPath)
+      properties.load(source.bufferedReader())
+      Right(properties)
+    catch
+      case e: Throwable => Left(e)
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,31 +1,35 @@
 import cats.effect.{ExitCode, IO, IOApp}
 import fs2.text
 import pipeline.pipeline.*
+import config.getConfig
 
 import java.io.File
 import scala.language.postfixOps
 
 object main extends IOApp {
 
-  val year = 2020
-  val month = 2
-  val urlBase = "https://transtats.bts.gov/PREZIP/"
-  val urlFile = s"On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${year}_$month.zip"
-  val outBase = new File(".").getCanonicalPath + "/resources/data/"
-  val outZip = "files.zip"
-  val outFile = "outs/"
-  val target = s"On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_${year}_$month.csv"
+  private val properties = getConfig match
+    case Left(e) => throw e
+    case Right(p) => p
+  private val projectRoot = new File(".").getCanonicalPath
 
-  val chunkSize = 1000
+  private def loadTranstats(month: Int, year: Int): fs2.Stream[IO, Unit] =
+    val src = downloadFlightFile(
+      properties.getProperty("target_url_base") + s"On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${year}_$month.zip",
+      projectRoot + properties.getProperty("data_directory_path") + s"${year}_$month.zip",
+      projectRoot + properties.getProperty("data_directory_path") + s"${year}_$month/",
+      s"On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)_${year}_$month.csv"
+    )
 
-  def run(args: List[String]): IO[ExitCode] =
-
-    val src = downloadFlightFile(urlBase + urlFile, outBase + outZip, outBase + outFile, target)
-    val pipe = src
+    src
       .through(text.utf8.decode)
-      .through(pipeToFlightChunk(chunkSize))
+      .through(pipeToFlightChunk(properties.getProperty("chunk_size").toInt))
       .through(filterNoneChunk)
       .through(flightSinkChunk)
 
-    pipe.compile.drain.as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] =
+    loadTranstats(properties.getProperty("month").toInt, properties.getProperty("year").toInt)
+      .compile
+      .drain
+      .as(ExitCode.Success)
 }
